f69be41f16db26a5f09f8caa5d30670f1e0dd877
Bustav01 <diegobenelohim@gmail.com>
2025-03-03 12:55:27 -0300

    Reordenar directorios

---
+++ 12_guessNumber/guessNumber.py
@@ -0,0 +1,13 @@
+'''
+TODO:Bienvenida,
+elegir número entre 1 y 100
+elegir dificultad
+mostrar intentos
+LOOP:
+    haz un intento
+    si incorrecto:
+        feedback (muy alto, muy bajo)
+    si correcto:
+        ganaste.
+    tienes N intentos
+'''
