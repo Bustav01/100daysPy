f6b490fdb16c1b605e1226a711e0c81844a838d5
Bustav01 <diegobenelohim@gmail.com>
2026-07-01 03:30:49 -0400

    Requirements and flow revision

--- 16_OOPCoffeeMachine/main.py
+++ 16_OOPCoffeeMachine/main.py
@@ -1,3 +1,5 @@
+# TODO: añadir recarga
+
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
@@ -8,19 +10,19 @@ def main():
     money = MoneyMachine()
 
     menu_items = menu.get_items()
-    serve = True
+    is_on = True
     #Loop pedido:
-    while serve == True:
+    while is_on == True:
         request = input(f"Menú: {menu_items}\n")
-        order = menu.find_drink(request)
-        print(request)
-        if request == None:
-            serve == False #Cortar loop
+        if request == 'off':
+            is_on = False
         elif request == 'report':
             coffee.report()
             money.report()
         else:
-            bill = order.cost
-            serve = money.make_payment(bill)
+            order = menu.find_drink(request)
+            if coffee.is_resource_sufficient(order) == True:
+                if money.make_payment(order.cost) == True:
+                    coffee.make_coffee(order)
 if __name__ == "__main__":
     main()
